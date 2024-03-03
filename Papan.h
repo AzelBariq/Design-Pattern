938b8ec712bd4221f140e1e6f426db704511cb33
AzelBariq <162020497+AzelBariq@users.noreply.github.com>
2024-03-03 22:20:24 +0700

    Create Papan.h

---
+++ Papan.h
@@ -0,0 +1,23 @@
+#ifndef BOARD_H
+#define BOARD_H
+
+#include <vector>
+#include <string>
+
+class Board {
+private:
+    std::vector<std::vector<int>> grid;
+
+public:
+    Board();
+    void print();
+    void loadChallenge(const std::string& filename);
+    bool isMoveValid(int row, int col, int value);
+    bool isRowValid(int row, int value);
+    bool isColValid(int col, int value);
+    bool isSubgridValid(int row, int col, int value);
+    void updateMove(int row, int col, int value);
+    int getValue(int row, int col);
+};
+
+#endif // BOARD_H
